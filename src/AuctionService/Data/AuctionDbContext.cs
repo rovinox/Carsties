cfce35866d56fada4466f026ea8c28a7e7357440
Neaj Mahmud <neajmahmud.dev@gmail.com>
2024-11-28 15:34:37 -0500

    first commit

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AuctionDbContext(DbContextOptions options) : DbContext(options)
+    {
+         public DbSet<Auction> Auctions { get; set; }
+    }
+}
