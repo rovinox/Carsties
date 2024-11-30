2e6c6b12660910c2f9a0e91323665d29fea6b8fc
Neaj Mahmud <neajmahmud.dev@gmail.com>
2024-11-29 19:10:28 -0500

    added search

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+{
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly IConfiguration _config = config;
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] 
+            + "/api/auctions?date=" + lastUpdated);
+    }
+}
